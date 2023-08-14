60d8b3457c40c7a1d0e5929627acb697bcd06a90
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-08-14 19:10:26 +0100

    baseline Updated Version

--- models/reading-store.js
+++ models/reading-store.js
@@ -1,67 +1,76 @@
-import { v4 } from "uuid";
 import { initStore } from "../utils/store-utils.js";
 
 const db = initStore("stations");
 
-// Function to determine the Beaufort level based on wind speed
 function getBeaufortLevel(windSpeed) {
-  if (windSpeed < 0.3) {
+  if (windSpeed < 0.5) {
     return 0;
-  } else if (windSpeed < 1.6) {
+  } else if (windSpeed <= 7) {
     return 1;
-  } else if (windSpeed < 3.4) {
+  } else if (windSpeed <= 13.8) {
     return 2;
-  } else if (windSpeed < 5.5) {
+  } else if (windSpeed <= 20.7) {
     return 3;
-  } else if (windSpeed < 8) {
+  } else if (windSpeed <= 28.5) {
     return 4;
-  } else if (windSpeed < 10.8) {
+  } else if (windSpeed <= 38.8) {
     return 5;
-  } else if (windSpeed < 13.9) {
+  } else if (windSpeed <= 49.9) {
     return 6;
-  } else if (windSpeed < 17.2) {
+  } else if (windSpeed <= 61.2) {
     return 7;
-  } else if (windSpeed < 20.8) {
+  } else if (windSpeed <= 74.5) {
     return 8;
-  } else if (windSpeed < 24.5) {
+  } else if (windSpeed <= 88.5) {
     return 9;
-  } else if (windSpeed < 28.5) {
+  } else if (windSpeed <= 102.8) {
     return 10;
-  } else if (windSpeed < 32.7) {
-    return 11;
   } else {
-    return 12;
+    return 11; // Maximum value for the provided ranges
   }
 }
-// Function to get the Beaufort level description based on wind speed
+
 function getBeaufortLevelDescription(windSpeed) {
-  if (windSpeed < 0.3) {
+  if (windSpeed < 0.5) {
     return 'Calm';
-  } else if (windSpeed < 1.6) {
-    return 'Light air';
-  } else if (windSpeed < 3.4) {
-    return 'Light breeze';
-  } else if (windSpeed < 5.5) {
-    return 'Gentle breeze';
-  } else if (windSpeed < 8) {
-    return 'Moderate breeze';
-  } else if (windSpeed < 10.8) {
-    return 'Fresh breeze';
-  } else if (windSpeed < 13.9) {
-    return 'Strong breeze';
-  } else if (windSpeed < 17.2) {
-    return 'Near gale';
-  } else if (windSpeed < 20.8) {
+  } else if (windSpeed <= 7) {
+    return 'Light Air';
+  } else if (windSpeed <= 13.8) {
+    return 'Light Breeze';
+  } else if (windSpeed <= 20.7) {
+    return 'Gentle Breeze';
+  } else if (windSpeed <= 28.5) {
+    return 'Moderate Breeze';
+  } else if (windSpeed <= 38.8) {
+    return 'Fresh Breeze';
+  } else if (windSpeed <= 49.9) {
+    return 'Strong Breeze';
+  } else if (windSpeed <= 61.2) {
+    return 'Near Gale';
+  } else if (windSpeed <= 74.5) {
     return 'Gale';
-  } else if (windSpeed < 24.5) {
-    return 'Strong gale';
-  } else if (windSpeed < 28.5) {
-    return 'Storm';
-  } else if (windSpeed < 32.7) {
-    return 'Violent storm';
+  } else if (windSpeed <= 88.5) {
+    return 'Severe Gale';
+  } else if (windSpeed <= 102.8) {
+    return 'Strong Storm';
   } else {
-    return 'Hurricane force';
+    return 'Violent Storm';
   }
+}
+
+function getWeatherDescription(code) {
+  const weatherDescriptions = {
+    100: "Clear",
+    200: "Partial clouds",
+    300: "Cloudy",
+    400: "Light Showers",
+    500: "Heavy Showers",
+    600: "Rain",
+    700: "Snow",
+    800: "Thunder",
+  };
+
+  return weatherDescriptions[code];
 }
 
 export const readingStore = {
@@ -69,12 +78,12 @@ export const readingStore = {
     await db.read();
     return db.data.readings;
   },
-  // Export the readingStore object containing async functions to interact with the database
+  
   async getLastReading() {
     await db.read();
     const lastReadings = db.data.stations.map((station) => {
       if (station.readings.length === 0) {
-        return null;
+        return { id: station.id, name: station.name, lastReading: null }; // Indicate no data
       }
       const lastReading = station.readings[station.readings.length - 1];
       const temperatureCelsius = lastReading.temperature;
@@ -82,6 +91,7 @@ export const readingStore = {
       const windSpeed = lastReading.windSpeed;
       const beaufortLevel = getBeaufortLevel(windSpeed);
       const beaufortDescription = getBeaufortLevelDescription(windSpeed);
+      const weatherDescription = getWeatherDescription(lastReading.code);
       return {
         id: station.id,
         name: station.name,
@@ -89,9 +99,9 @@ export const readingStore = {
         temperatureFahrenheit: temperatureFahrenheit,
         beaufortLevel: beaufortLevel,
         beaufortDescription: beaufortDescription,
+        weatherDescription: weatherDescription,
       };
     }); 
     return lastReadings;
   },
-  
 };
